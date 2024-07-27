eda347a039d587517306f46a4667db2f14ae242a
Elias Batista <eliasbatistasouza@gmail.com>
2024-07-27 20:25:18 -0300

    Fim da aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,22 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/homework", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def complete_homework():
+    return """
+    <hmtl>
+    <title>Tarefa de Casa</title>
+    <h1>Trabalho Feito</h1>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,19 @@ def test_root_deve_retornar_ok_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello, World!"}
+
+
+def test_homework():
+    client = TestClient(app)
+
+    response = client.get("/homework")
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <hmtl>
+    <title>Tarefa de Casa</title>
+    <h1>Trabalho Feito</h1>
+    </html>"""
+    )
