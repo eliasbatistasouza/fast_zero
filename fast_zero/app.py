1b06191ee347143a39731fe08a19b49492f6b6c2
Elias Batista <contato@eliasbatista.com>
2024-08-04 12:51:22 -0300

    Add get user and write tests for user not found

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -43,7 +43,9 @@ def update_user(user_id: int, user: UserSchema):
     return user_with_id
 
 
-@app.delete('/users/{user_id}', response_model=Message)
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
@@ -53,3 +55,14 @@ def delete_user(user_id: int):
     del database[user_id - 1]
 
     return {'message': 'User deleted'}
+
+
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    return database[user_id - 1]
--- tests/test_app.py
+++ tests/test_app.py
@@ -14,9 +14,9 @@ def test_create_user(client):
     response = client.post(
         '/users/',
         json={
-            'username': 'testusername',
-            'email': 'test@email.com',
-            'password': 'testpassword',
+            'username': 'elias',
+            'email': 'elias@email.com',
+            'password': 'minhasenha',
         },
     )
 
@@ -24,8 +24,8 @@ def test_create_user(client):
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         'id': 1,
-        'username': 'testusername',
-        'email': 'test@email.com',
+        'username': 'elias',
+        'email': 'elias@email.com',
     }
 
 
@@ -36,11 +36,22 @@ def test_read_users(client):
     assert response.json() == {
         'users': [
             {
-                'username': 'testusername',
-                'email': 'test@email.com',
+                'username': 'elias',
+                'email': 'elias@email.com',
                 'id': 1,
             }
         ]
+    }
+
+
+def test_read_user(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'elias',
+        'email': 'elias@email.com',
+        'id': 1,
     }
 
 
@@ -48,22 +59,50 @@ def test_update_user(client):
     response = client.put(
         '/users/1',
         json={
-            'username': 'bob',
-            'email': 'bob@email.com',
-            'password': 'mypassword',
+            'username': 'giselle',
+            'email': 'giselle@email.com',
+            'password': 'outrasenha',
         },
     )
     # Testar status code 404
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-        'username': 'bob',
-        'email': 'bob@email.com',
+        'username': 'giselle',
+        'email': 'giselle@email.com',
         'id': 1,
     }
 
 
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'giselle',
+            'email': 'giselle@email.com',
+            'password': 'outrasenha',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user(client):
     response = client.delete('/users/1')
-    # Testar status code 404
+
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_read_user_not_found(client):
+    response = client.get('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
