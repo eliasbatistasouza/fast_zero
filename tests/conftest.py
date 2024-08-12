8e3a26dd8c63e609a6cc77e41afdde8b02653850
Elias Batista <contato@eliasbatista.com>
2024-08-11 23:30:27 -0300

    Refactor client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.database import get_session
 from fast_zero.models import table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
