da0b9774612c09f7d22c4dd4422ed696f9040381
Elias Batista <contato@eliasbatista.com>
2024-09-01 19:07:08 -0300

    Add token fixture and refactor user fixture to cover user authorization

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from sqlalchemy.pool import StaticPool
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import User, table_registry
+from fast_zero.security import get_password_hash
 
 
 @pytest.fixture()
@@ -38,9 +39,27 @@ def session():
 
 @pytest.fixture()
 def user(session):
-    user = User(username='test', email='test@test.com', password='testtest')
+    password = 'testtest'
+
+    user = User(
+        username='test',
+        email='test@test.com',
+        password=get_password_hash(password),
+    )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = 'testtest'  # Monkey Patch
+
     return user
+
+
+@pytest.fixture()
+def token(client, user):
+    response = client.post(
+        '/token/',
+        data={'username': user.username, 'password': user.clean_password},
+    )
+
+    return response.json()['access_token']
